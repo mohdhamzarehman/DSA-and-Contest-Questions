dcd66efb876a64a4cc7f70535919fdf73bd2a8b8
Mohammad Hamza <134505680+mohdhamzarehman@users.noreply.github.com>
2024-03-06 23:42:36 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,12 @@
+class Solution {
+     public static String helper(String num, int i) {
+        if(i<=0)
+            return "";
+        if ((num.charAt(i - 1) - '0') % 2 == 1)
+            return num;
+        return helper(num.substring(0,i-1),i-1);
+    }
+    public static String largestOddNumber(String num) {
+        return helper(num,  num.length());
+    }
+}
