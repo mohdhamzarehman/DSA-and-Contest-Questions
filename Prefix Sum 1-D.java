7c5e0c3c13b932697c2fef819dfbab154e6e565a
Mohammad Hamza <134505680+mohdhamzarehman@users.noreply.github.com>
2024-05-06 11:19:56 +0530

    Create Prefix Sum 1-D.java

---
+++ Prefix Sum 1-D.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+
+        // calculate cumsum
+        ArrayList<Integer> cumArr = new ArrayList<Integer>();
+        cumArr.add(arr.get(0));
+        for (int i = 1; i < n; i++) {
+            cumArr.add(cumArr.get(i - 1) + arr.get(i));
+        }
+
+        int q = scanner.nextInt();
+        while (q-- > 0) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            if (start == 0) {
+                System.out.println(cumArr.get(end));
+            } else {
+                System.out.println(cumArr.get(end) - cumArr.get(start - 1));
+            }
+        }
+    }
+}
